ef2564bc6e0a0bdf3a4f5211f238e88d9279a759
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2022-10-06 11:43:44 +0300

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Atakan Kömürcü on 6.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
