90422a7f40fabb85ff99b178bf23aafd6b1b7fd3
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2022-10-07 18:54:57 +0300

    Tablolar ekleme ve silme eklendi

--- Expenses/ContentView.swift
+++ Expenses/ContentView.swift
@@ -8,19 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Enviromental Variables
+    @Environment(\.managedObjectContext) var managedObjContext
+    
+    @FetchRequest(entity: Tablo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Tablo.name, ascending: true)]) var tablolar: FetchedResults<Tablo>
+    
+    // MARK: Variables
+    @State private var presentAlert = false
+    @State private var tabloAd = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List{
+                ForEach(tablolar,id: \.self){tablo in
+                    Text("\(tablo.name ?? "Unknown")")
+                }
+                .onDelete(perform: removeItem)
+            }
+            
+            .navigationTitle("Tablolar")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: Button("Ekle"){
+                presentAlert.toggle()
+            })
+            .alert("Tablo Oluşturma", isPresented: $presentAlert, actions: {
+                TextField("Tablo ismi", text: $tabloAd)
+                
+                Button("Ekle", action: {
+                    addTablo(name: tabloAd)
+                    tabloAd = ""
+                })
+                Button("Cancel", role: .cancel, action: {})
+            })
+            
+            
         }
-        .padding()
+
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = tablolar[index]
+            PersistenceController.shared.delete(item)
+        }
+    }
+    
+    func addTablo(name: String){
+        let tablo = Tablo(context: managedObjContext)
+        tablo.name = tabloAd
+        PersistenceController.shared.save()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
