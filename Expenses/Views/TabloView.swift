20726ca900e6b200ca8b4e94c07fdb513562f20b
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2022-10-26 17:03:46 +0300

    Tablodan gider silme eklendi

--- Expenses/Views/TabloView.swift
+++ Expenses/Views/TabloView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TabloView: View {
     
+    // TODO: Girilen tablonun adı çekilecek
     var tabloAd = "Şimdilik Boş"
     
     @Environment(\.managedObjectContext) var managedObjContext
@@ -39,19 +40,31 @@ struct TabloView: View {
             List{
                 ForEach(giderler) {gider in
                     VStack{
-                        Text("\(gider.name ?? "unknown")")
-                        Text("\(gider.tarih?.formatted(.dateTime) ?? Date().formatted(.dateTime))")
+                        HStack{
+                            Text("\(gider.name ?? "unknown")")
+                            Spacer()
+                            Text("\(gider.tarih?.formatted(.dateTime) ?? Date().formatted(.dateTime))")
+                        }
                         Text("\(gider.tabloID ?? "Unknown")")
+                            .font(.caption)
                     }
                 }
+                .onDelete(perform: removeItem)
             }
         }
         .navigationTitle("\(tabloAd)")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing: Button("Ekle"){
-            // TODO: Gider eklemek için fonksiyon
+            // TODO: Gider eklemek için açılacak olan sheet eklenecek
             addGider()
         })
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = giderler[index]
+            PersistenceController.shared.delete(item)
+        }
     }
 }
 
